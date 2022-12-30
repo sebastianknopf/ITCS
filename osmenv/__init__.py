2bfff26deccbb88f02045609f6e6fd226513e104
sebastianknopf <43302817+sebastianknopf@users.noreply.github.com>
2022-12-30 16:32:56 +0100

    cleaned up repository for new version development

--- osmenv/__init__.py
+++
@@ -1,7 +0,0 @@
-import gym
-
-gym.envs.register(
-    id='OSMEnvironment',
-    entry_point='osmenv.env:OSMEnvironment',
-    max_episode_steps=100
-)
--- simulation.py
+++ simulation.py
@@ -1,6 +1,4 @@
-
-
-def _load_trip_ways(data_file):
+def _load_trip_nodes(data_file):
     # read file line by line and pass ways as ordered in file
     with open(data_file) as file:
         ways = [int(line.rstrip()) for line in file]
@@ -9,17 +7,17 @@ def _load_trip_ways(data_file):
 
 _trip743h = {
     'id': '743-H',
-    'ways': _load_trip_ways('data/trip743h.txt')
+    'ways': _load_trip_nodes('data/land2.seq')
 }
 
 _trip744h = {
     'id': '744-H',
-    'ways': _load_trip_ways('data/trip744h.txt')
+    'ways': _load_trip_nodes('data/land1.seq')
 }
 
 _trip2h = {
     'id': '2-H',
-    'ways': _load_trip_ways('data/trip2h.txt')
+    'ways': _load_trip_nodes('data/city.seq')
 }
 
 trips = [_trip2h, _trip743h, _trip744h]
